477fe958321377d7c5e7a69e2e8350f3c274a15f
Sene68 <senefull68@gmail.com>
2022-11-07 21:12:25 +0900

    Initial commit

---
+++ src/main/kotlin/com/sample/petclinic/controller/WelcomeController.kt
@@ -0,0 +1,14 @@
+package com.sample.petclinic.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+class WelcomeController {
+
+    @GetMapping("/")
+    fun welcome(): String {
+        return "welcome"
+    }
+}
---
+++ src/test/kotlin/com/sample/petclinic/PetclinicApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sample.petclinic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PetclinicApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
