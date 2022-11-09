fa775058cf51d00f6cbd98e4af51b72205669b30
Sene68 <senefull68@gmail.com>
2022-11-09 22:37:41 +0900

    h2 database 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,10 +19,13 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	runtimeOnly("com.h2database:h2")
 }
 
 tasks.withType<KotlinCompile> {
